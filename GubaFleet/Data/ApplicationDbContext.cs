0d22ad0b85685ca2115cb12eb411b79a8458b186
Brook <felekebrook05@gmail.com>
2023-08-03 11:26:37 +0300

    added application db context

---
+++ GubaFleet/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GubaFleet.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+    }
+}
